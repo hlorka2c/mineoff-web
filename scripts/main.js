c2bb4c65f72c26e054c0e7acb3fa3ad637910628
hlorka2c <arbuzzzzz44@mail.ru>
2024-09-24 22:01:12 +0300

    fix overflow body

--- scripts/main.js
+++ scripts/main.js
@@ -6,11 +6,13 @@ const serverDOM = document.querySelector('.offer__inner-server'),
 serverDOM.addEventListener('click', function (event) {
     setTimeout(showModal(modalDOM), 500);
     event.target.classList.add(activeServerDOMClass);
+    document.body.style.overflow = 'hidden';
 
     const closeDOM = modalDOM.querySelector('.modal__header-close');
     closeDOM.addEventListener('click', function () {
         hideModal(modalDOM);
         serverDOM.classList.remove(activeServerDOMClass)
+        document.body.style.overflow = '';
     })
 });
 
